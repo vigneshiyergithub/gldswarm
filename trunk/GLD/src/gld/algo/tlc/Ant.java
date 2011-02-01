090a95ae0358fb7584e4337b9722135932cfdd58
jonalejo@gmail.com <jonalejo@gmail.com@4190669a-aa54-338d-39d3-a9034d5fdd67>
2011-02-01 23:09:58 +0000

    Versión no funcional - very buggy Finalizar la implementación del algoritmo, falta actualización de umbrales Liberar zonas de agentes

--- trunk/GLD/src/gld/algo/tlc/Ant.java
+++ trunk/GLD/src/gld/algo/tlc/Ant.java
@@ -11,30 +11,62 @@ import java.lang.Math;
  * @author Jonathan
  */
 public class Ant {
+    final static int RS = 5;
     public float FOREING_FACTOR = 0.5f; //Factor of performance for foreing agents
     private Swarm swarm;
-    private Float [] tresholds;
-    private boolean available;
-    private Task [] tasks;
+    private Float [][] tresholds;
+    private int available;
+    private Site [][] zones;
     private int currentTask;
     Random ran;
     private float action;
 
-    public Ant(Swarm swarm){
+    public Ant(Swarm swarm, Site [][] zones){
         int numTasks = Task.numTasks;
-        tresholds = new Float[numTasks];
+        this.zones = zones;
+        this.swarm = swarm;
+        
+        tresholds = new Float[zones.length][];
         for(int i = 0; i < tresholds.length; i++){
-            tresholds[i] = swarm.getInitial_treshold();
+            tresholds[i] = new Float[zones[i].length];
         }
+
+        for(int i = 0; i < tresholds.length; i++){
+            for(int j = 0; j < tresholds[i].length; j++){
+                tresholds[i][j] = swarm.getInitial_treshold();
+            }
+        }
+
         this.action = swarm.getActionPerAgent();
-        this.available = true;
+        this.available = 0;
         //this.tasks = tasks;
         ran = new Random();
         currentTask = (int) Math.floor(numTasks*ran.nextFloat());
     }
 
     public void step(){
-        double random = Math.random();
+        double random;
+        Site [] workZone;
+        random = Math.floor((zones.length)*Math.random());
+        workZone = zones[(int)random];
+        double [] pi = new double[workZone.length];
+        double [] r = new double[workZone.length];
+        double Sj2, th2;
+
+        if(this.isAvailable()){
+            for(int i = 0; i < workZone.length; i++){
+                r[i] = Math.random();
+                Sj2 = Math.pow(workZone[i].getDemand(),2);
+                th2 = Math.pow(tresholds[(int)random][i], 2);
+                pi[i] = (Sj2)/(Sj2 + swarm.getAlpha()*th2);
+
+                if(r[i] < pi[i]){
+                    workZone[i].addAgent(this);
+                    this.unavailable(5);
+                }
+            }
+        }
+
         
     }
 
@@ -42,21 +74,25 @@ public class Ant {
         this.currentTask = newTask;
     }
 
+    public void unavailable(int time){
+        available = time;
+    }
+
     public boolean isAvailable() {
-        return available;
+        available--;
+        if(available <= 0)
+            return true;
+        else
+            return false;
     }
 
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
+    //    public float getTreshold(int numTask) {
+//        return tresholds[numTask];
+//    }
 
-    public float getTreshold(int numTask) {
-        return tresholds[numTask];
-    }
-
-    public void setTreshold(int numTask, float treshold) {
-        this.tresholds[numTask] = treshold;
-    }
+//    public void setTreshold(int numTask, float treshold) {
+//        this.tresholds[numTask] = treshold;
+//    }
 
     public float getAction() {
         return action;
